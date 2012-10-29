313cb923a56813dfb266647966bcd09245f528a1
Carlos Delfino Carvalho Pinheiro <consultoria@carlosdelfino.eti.br>
2012-10-29 20:21:30 -0200

    Nova versão do DuinOS preadaptada para a IDE 1.5 do Arduino. Este release não está pronto precisando ser testado para verificar erros e ausencias de arquivos importantes. Este release do DuinOS ainda não funciona com o Arduino DUE

---
+++ arduino.DuinOS.AVR/main.cpp
@@ -0,0 +1,34 @@
+#include <Arduino.h>
+
+unsigned portBASE_TYPE mainLoopPriority;
+
+void main_Task(void *pvParameters)
+{
+	for (;;) {
+		loop();
+		if (serialEventRun) serialEventRun();
+	}
+        
+}
+
+int main(void)
+{
+
+	mainLoopPriority = LOW_PRIORITY;
+
+	init();
+
+#if defined(USBCON)
+	USBDevice.attach();
+#endif
+	
+	setup();
+    
+	xTaskCreate(main_Task, (signed portCHAR *) "main", configMINIMAL_STACK_SIZE, NULL, mainLoopPriority, NULL);
+	vTaskStartScheduler();
+
+	for(;;);
+
+	return 0;
+}
+
