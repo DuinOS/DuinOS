1db6c11d2c8de6a1407ee6f17d1911d3fdafe4cc
Carlos Delfino Carvalho Pinheiro <consultoria@carlosdelfino.eti.br>
2012-11-03 14:59:56 -0200

    Inserido as macros que delimitam a area critica quando usando lcd.write(const char*). Ainda estou fazendo testes, continuo tendo problemas de travamento quando uso lcd.write(uint8_t,mode), Já tentei de diversas formas delimitando a area critica, usando delays() mas não obtive sucesso ainda.

---
+++ examples/DuinOS/LCDMultiTask/LCDMultiTask.ino
@@ -0,0 +1,49 @@
+
+#include <LiquidCrystal2.h>
+
+
+
+//LiquidCrystal lcd(10,11,2,3,4,5);
+LiquidCrystal2 lcd(10,11,22,24,26,28);
+
+void setup(){
+    lcd.begin(16,2);
+    
+    pinMode(8,OUTPUT);
+    pinMode(9,OUTPUT);
+
+    analogWrite(8,200); // contraste
+    analogWrite(9,128); // brilho de fundo
+    lcd.write("1234567890123456");
+}
+
+volatile int x = 0;
+void loop(){
+    //lcd.write("1234567890123456");
+    //lcd.write("1234567890123456");
+    
+    
+delay(3);
+    lcd.setCursor(0,1);
+delay(3);
+
+//taskENTER_CRITICAL();
+lcd.write(x++);
+//taskEXIT_CRITICAL();     
+
+lcd.print("teste ");
+
+    for(int i=100;i<=200;i++) {
+        analogWrite(8,i);
+        delay(15);
+    }
+    for(int i=200;i>=100;i--) {
+        analogWrite(8,i);
+        delay(15);
+    }
+    
+    
+     
+}
+
+
