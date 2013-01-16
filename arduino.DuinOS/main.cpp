982b12f2df8adc4a58367a25c648eae8a495ab82
Carlos Delfino Carvalho Pinheiro <consultoria@carlosdelfino.eti.br>
2013-01-16 14:23:19 -0200

    ajustado o esqueleto da aplicação para que a função setup() possa usar delay().
    Este código foi perdido e está agora restaurado.

--- arduino.DuinOS/main.cpp
+++ arduino.DuinOS/main.cpp
@@ -1,33 +1,58 @@
 #include <Arduino.h>
 
+
+unsigned portBASE_TYPE mainSetupPriority;
+unsigned portBASE_TYPE mainSetupStackSize;
 unsigned portBASE_TYPE mainLoopPriority;
+unsigned portBASE_TYPE mainLoopStackSize;
+
+xTaskHandle xHandleLoop;
+xTaskHandle xHandleSetup;
 
 void main_Task(void *pvParameters)
 {
+#if INCLUDE_vTaskDelete
+	vTaskDelete(xHandleSetup);
+#endif
 	for (;;) {
 		loop();
 		if (serialEventRun) serialEventRun();
 	}
-        
+		
+} 
+
+void setup_Task(void *pvParameters)
+{
+
+	setup();
+	vTaskResume(xHandleLoop);
+	vTaskSuspend(NULL);
+
 }
 
-int main(void) 
+int main(void)
 {
-	mainLoopPriority = LOW_PRIORITY;
-
+	mainSetupPriority  = HIGH_PRIORITY;
+	mainSetupStackSize = configMINIMAL_STACK_SIZE * 2;
+	mainLoopPriority   = LOW_PRIORITY;
+	mainLoopStackSize  = configMINIMAL_STACK_SIZE;
 	init();
 
 #if defined(USBCON)
 	USBDevice.attach();
 #endif
-	
-	setup();
 
-	xTaskCreate(main_Task, (signed portCHAR *) "main", configMINIMAL_STACK_SIZE, NULL, mainLoopPriority, NULL);
+//	setup();
+
+	xTaskCreate(setup_Task, (signed portCHAR *) "setup", mainSetupStackSize, NULL, mainSetupStackSize, &xHandleSetup);
+
+	xTaskCreate(main_Task, (signed portCHAR *) "main", mainLoopStackSize, NULL, mainLoopPriority, &xHandleLoop);
+	vTaskSuspend(xHandleLoop);
+
 	vTaskStartScheduler();
 
-	for(;;);
-
+	for (;;);
+        
 	return 0;
 }
 
