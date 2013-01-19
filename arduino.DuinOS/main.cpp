7fe5a4463e266a10a3c392eb0c47cde02427dcdb
Carlos Delfino Carvalho Pinheiro <consultoria@carlosdelfino.eti.br>
2013-01-19 09:36:47 -0200

    atualização do esqueleto do código para permitir o uso da função delay() dentro do setup. Estou trabalhando no código para permitir a auto seleção do tipo de delay a ser usado quando usando ou não as tasks.

--- arduino.DuinOS/main.cpp
+++ arduino.DuinOS/main.cpp
@@ -1,19 +1,16 @@
 #include <Arduino.h>
+#include <FreeRTOSConfig.h>
 
-
-unsigned portBASE_TYPE mainSetupPriority;
-unsigned portBASE_TYPE mainSetupStackSize;
-unsigned portBASE_TYPE mainLoopPriority;
-unsigned portBASE_TYPE mainLoopStackSize;
+unsigned portBASE_TYPE mainSetupPriority  = HIGH_PRIORITY;
+unsigned portBASE_TYPE mainSetupStackSize = configMINIMAL_STACK_SIZE * 2;
+unsigned portBASE_TYPE mainLoopPriority   = LOW_PRIORITY;
+unsigned portBASE_TYPE mainLoopStackSize  = configMINIMAL_STACK_SIZE;
 
 xTaskHandle xHandleLoop;
 xTaskHandle xHandleSetup;
 
 void main_Task(void *pvParameters)
 {
-#if INCLUDE_vTaskDelete
-	vTaskDelete(xHandleSetup);
-#endif
 	for (;;) {
 		loop();
 		if (serialEventRun) serialEventRun();
@@ -25,29 +22,27 @@ void setup_Task(void *pvParameters)
 {
 
 	setup();
-	vTaskResume(xHandleLoop);
-	vTaskSuspend(NULL);
+
+	xTaskCreate(main_Task, (signed portCHAR *) "main", mainLoopStackSize, NULL, mainLoopPriority, &xHandleLoop);
+
+#if INCLUDE_vTaskDelete
+	vTaskDelete(xHandleSetup);
+#else
+	while(true) vTaskSuspend(NULL);
+#endif
 
 }
 
 int main(void)
 {
-	mainSetupPriority  = HIGH_PRIORITY;
-	mainSetupStackSize = configMINIMAL_STACK_SIZE * 2;
-	mainLoopPriority   = LOW_PRIORITY;
-	mainLoopStackSize  = configMINIMAL_STACK_SIZE;
+
 	init();
 
 #if defined(USBCON)
 	USBDevice.attach();
 #endif
 
-//	setup();
-
 	xTaskCreate(setup_Task, (signed portCHAR *) "setup", mainSetupStackSize, NULL, mainSetupStackSize, &xHandleSetup);
-
-	xTaskCreate(main_Task, (signed portCHAR *) "main", mainLoopStackSize, NULL, mainLoopPriority, &xHandleLoop);
-	vTaskSuspend(xHandleLoop);
 
 	vTaskStartScheduler();
 
